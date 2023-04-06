82a5546a85f841d75063420b3c7f848a3bd6a0a7
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-04-06 23:41:48 +0900

    #1 putのmethodのリクエストパラメータにmemoを追加

--- src/resources/js/components/ItemManagement/tool/EditItem.js
+++ src/resources/js/components/ItemManagement/tool/EditItem.js
@@ -1,15 +1,15 @@
-import React, { useState, useContext } from 'react';
+import React, {useState, useContext} from 'react';
 import Box from '@mui/material/Box';
 import EditButton from '../../common/EditButton';
-import { NoticeContext } from '../../common/Notification';
+import {NoticeContext} from '../../common/Notification';
 import axios from 'axios';
-import { value_validation } from '../../common/tool';
+import {value_validation} from '../../common/tool';
 
 // アイテム編集機能 //
 // アイテムの編集ボタンを押すとアイテムを編集する画面が表示され
 // 閉じるまたは編集ボタンを押すとアイテム編集のキャンセルまたはアイテム編集が完了する
 // 入力は1字以上200字以下で制限する
-const EditItem = ({ folderId, item, handleReload }) => {
+const EditItem = ({folderId, item, handleReload}) => {
     const [open, setOpen] = useState(false);
     const [error, setError] = useState(false);
     const [value, setValue] = useState("");
@@ -44,36 +44,39 @@ const EditItem = ({ folderId, item, handleReload }) => {
 
     const handleChange = (e) => {
         setValue(e.target.value);
-        if(value_validation(e.target.value)) {
+        if (value_validation(e.target.value)) {
             handleErrorRefresh();
-        }
-        else {
+        } else {
             handleError(errorMessage);
         }
     };
 
     const handleSubmit = () => {
-        if(value_validation(value)) {
+        if (value_validation(value)) {
             updateItem();
             handleClose();
-        }
-        else {
+        } else {
             handleError(errorMessage);
         }
     }
 
     // API通信後に成功かエラーかを通知するための関数
     const ApiAfterAction = (payload) => {
-        dispatch({ type: 'update_message', payload: payload });
-        dispatch({ type: 'handleNoticeOpen' });
+        dispatch({type: 'update_message', payload: payload});
+        dispatch({type: 'handleNoticeOpen'});
         handleReload();
     }
 
     const updateItem = () => {
         const abortCtrl = new AbortController()
-        const timeout = setTimeout(() => { abortCtrl.abort() }, 10000);
+        const timeout = setTimeout(() => {
+            abortCtrl.abort()
+        }, 10000);
         axios
-            .put(`/api/folders/${folderId}/items/${item.id}`, { name: value.trim() }, { signal: abortCtrl.signal })
+            .put(`/api/folders/${folderId}/items/${item.id}`, {
+                name: nameValue.trim(),
+                memo: memoValue
+            }, {signal: abortCtrl.signal})
             .then(() => {
                 ApiAfterAction(`アニメ(${item.name})の更新が完了しました`);
             })
@@ -91,15 +94,15 @@ const EditItem = ({ folderId, item, handleReload }) => {
                 task_name="アニメの編集"
                 id="edit item"
                 label="新しいアニメ名"
-                open={ open }
-                error={ error }
-                errorText={ errorText }
-                handleClickOpen={ handleClickOpen }
-                handleChange={ handleChange }
-                handleClose={ handleClose }
-                handleSubmit={ handleSubmit }
-                handleRefresh={ handleRefresh }
-                value={ value }
+                open={open}
+                error={error}
+                errorText={errorText}
+                handleClickOpen={handleClickOpen}
+                handleChange={handleChange}
+                handleClose={handleClose}
+                handleSubmit={handleSubmit}
+                handleRefresh={handleRefresh}
+                value={value}
                 submit_button_name="完了"
                 aria_label="edit_item"
                 size="small"
