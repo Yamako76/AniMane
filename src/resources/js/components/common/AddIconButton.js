439b196cb46672d33150b5ffb0ca3a634d84125f
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-04-07 20:05:41 +0900

    #1 EditItemDialogを追加しました。

--- src/resources/js/components/common/AddIconButton.js
+++ src/resources/js/components/common/AddIconButton.js
@@ -3,37 +3,64 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import AddIcon from '@mui/icons-material/Add';
-import EditDialog from './EditDialog';
+import EditItemDialog from "./EditItemDialog";
 
 // Objectを追加するためのDialogを表示するボタン
 // Objectの追加ボタンを押すと新しいObject作成する画面が表示され
 // 閉じるまたは追加ボタンを押すと新しいObject作成のキャンセルまたは新しいObject作成が完了する
-const AddIconButton = ({ task_name, id, label, open, error, errorText, handleClickOpen, handleChange, handleClose, handleSubmit, handleRefresh, value, submit_button_name, sx }) => {
+const AddIconButton = ({
+                           taskName,
+                           id,
+                           label,
+                           open,
+                           error,
+                           errorText,
+                           handleClickOpen,
+                           handleChange,
+                           handleClose,
+                           handleSubmit,
+                           handleRefresh,
+                           value,
+                           submitButtonName,
+                           sx,
+                           memoName,
+                           memoId,
+                           memoLabel,
+                           memoValue,
+                           itemHandleChange,
+                           itemHandleRefresh
+                       }) => {
     return (
         <Box>
-            <Tooltip title={ task_name }>
+            <Tooltip title={taskName}>
                 <IconButton
-                    onClick={ handleClickOpen }
-                    disableFocusRipple={ true }
-                    sx={ sx }
+                    onClick={handleClickOpen}
+                    disableFocusRipple={true}
+                    sx={sx}
                 >
-                    <AddIcon />
+                    <AddIcon/>
                 </IconButton>
             </Tooltip>
 
-            <EditDialog
-                task_name={ task_name }
-                id={ id }
-                label={ label }
-                open={ open }
-                error={ error }
-                errorText={ errorText }
-                handleChange={ handleChange }
-                handleClose={ handleClose }
-                handleSubmit={ handleSubmit }
-                handleRefresh={ handleRefresh }
-                value={ value }
-                submit_button_name={ submit_button_name }
+            <EditItemDialog
+                taskName={taskName}
+                id={id}
+                label={label}
+                open={open}
+                error={error}
+                errorText={errorText}
+                handleChange={handleChange}
+                handleClose={handleClose}
+                handleSubmit={handleSubmit}
+                handleRefresh={handleRefresh}
+                value={value}
+                submitButtonName={submitButtonName}
+                memoName={memoName}
+                memoLabel={memoLabel}
+                memoValue={memoValue}
+                memoId={memoId}
+                itemHandleChange={itemHandleChange}
+                itemHandleRefresh={itemHandleRefresh}
             />
         </Box>
     );
