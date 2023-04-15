1664e02137db35106d6e59647670edc142f0e6ee
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-04-16 00:54:25 +0900

    #1 アイテム詳細ページを作成しました。

--- src/resources/js/pages/Item.js
+++ src/resources/js/pages/Item.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Box from "@mui/material/Box";
 import {useParams} from "react-router-dom";
+import ItemDetailManagement from "../components/ItemDetailManagement/ItemDetailManagement";
+import BackButton from "../components/ItemDetailManagement/BackButton";
+import Grid from "@mui/material/Grid";
 
 const Item = () => {
-    const { itemId } = useParams();
+    const {itemId} = useParams();
+    const {folderId} = useParams()
+    const itemSx = {
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+    };
+
     return (
         <Box>
-            {itemId}
+            <Box sx={{position: 'absolute', left: 50}}>
+                <BackButton folderId={folderId}/>
+            </Box>
+            <Box sx={itemSx}>
+                <Grid>
+                    <ItemDetailManagement itemId={itemId} folderId={folderId}/>
+                </Grid>
+            </Box>
         </Box>
     );
 };
