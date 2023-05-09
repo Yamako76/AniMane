e3e5463f51696ce86332489dfa19e96ea13fac5d
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-05-09 21:49:34 +0900

    #1 unit testの微調整を行いました。

--- src/tests/Feature/RoutingTest.php
+++ src/tests/Feature/RoutingTest.php
@@ -2,40 +2,44 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
 class RoutingTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
-    public function setUp() :void
+    public function setUp(): void
     {
         parent::setUp();
         $this->user = User::factory()->create();
     }
 
-    // 未認証ユーザーはtopページへアクセス可能
-    public function test_unAuthUser_can_see_top()
+    /**
+     * 未認証ユーザーはtopページを閲覧できることをテスト
+     */
+    public function test_unAuthUser_can_see_top_page(): void
     {
         $response = $this->get('/');
         $response->assertOk();
         $response->assertViewIs('top.index');
     }
 
-    // すでにログインしたユーザーはtopページではなく/app/homeへ転送
-    public function test_AuthUser_can_be_redirected_to_home()
+    /**
+     * ログイン済ユーザーはtopページではなく/app/homeにリダイレクトされることをテスト
+     */
+    public function test_authUser_can_be_redirected_to_home_page()
     {
         $response = $this->actingAs($this->user)->get('/');
         $response->assertStatus(302);
         $response->assertRedirect('/app/home');
     }
 
-    // 未認証ユーザーは/app/homeにアクセスできない
-    // その場合, ログインを要求
-    public function test_unAuthUser_cannot_access_home()
+    /**
+     * 未認証ユーザーは/app/homeにアクセスできないことをテスト
+     */
+    public function test_unAuthUser_cannot_access_home_page(): void
     {
         $response = $this->get('/app/home');
         $response->assertStatus(302);
--- src/tests/Unit/FolderPolicyTest.php
+++ src/tests/Unit/FolderPolicyTest.php
@@ -2,31 +2,38 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-use App\Policies\FolderPolicy;
 use App\Models\Folder;
 use App\Models\User;
+use App\Policies\FolderPolicy;
+use PHPUnit\Framework\TestCase;
 
 class FolderPolicyTest extends TestCase
 {
     /**
-     * FolderPolicyのバリデーション機能のUnitテスト
-     *
-     * @return void
+     * FolderPolicyのバリデーション機能テスト
+     * - UserのIDとFolderのuserIdが一致する場合
      */
-    public function test_OK_view()
+    public function testView_valid_user(): void
     {
-        $folder_policy = new FolderPolicy();
+        $folderPolicy = new FolderPolicy();
         $user = new User();
         $user->id = 1;
         $folder = new Folder();
         $folder->user_id = 1;
-        $this->assertTrue($folder_policy->view($user, $folder));
+        $this->assertTrue($folderPolicy->view($user, $folder));
+    }
 
+    /**
+     * FolderPolicyのバリデーション機能テスト
+     * - UserのIDとFolderのuserIdが一致しない場合
+     */
+    public function testView_invalid_user(): void
+    {
+        $folderPolicy = new FolderPolicy();
         $user = new User();
         $user->id = 1;
         $folder = new Folder();
         $folder->user_id = 2;
-        $this->assertFalse($folder_policy->view($user, $folder));
+        $this->assertFalse($folderPolicy->view($user, $folder));
     }
 }
